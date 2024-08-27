9e76b5679846e69d0793f7588501b319ade3b769
Yug-spec <vidiyala.yugander@viacom18.com>
2024-08-27 16:17:10 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,121 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.newsforu"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.newsforu"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    //Splash Api
+    implementation("androidx.core:core-splashscreen:1.2.0-alpha01")
+
+    implementation ("androidx.work:work-runtime-ktx:2.7.0")
+
+    //Navigation Compose
+    val hiltVersion = "1.2.0"
+    implementation("androidx.hilt:hilt-work:1.2.0")
+    implementation ("androidx.work:work-runtime-ktx:2.7.0")
+    kapt("androidx.hilt:hilt-compiler:$hiltVersion")
+    implementation("androidx.hilt:hilt-navigation-compose:$hiltVersion")
+
+    //Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-android-compiler:2.52")
+
+    //Retrofit
+    //Retrofit
+   implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //coil
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    //Datastore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    //compose Foundation
+    implementation("androidx.compose.foundation:foundation:1.6.8")
+
+    //Accompanist
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.31.4-beta")
+
+    //paging3
+    val paging_version = "3.3.2"
+    implementation("androidx.paging:paging-runtime-ktx:$paging_version")
+    implementation("androidx.paging:paging-compose:$paging_version")
+
+
+    //Room
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+}
+
+
+kapt {
+    correctErrorTypes = true
+}
---
+++ app/src/main/java/com/example/newsforu/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.example.newsforu.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.newsforu.data.remote.dto.NewsApi
+import com.example.newsforu.data.remote.dto.NewsPagingSource
+import com.example.newsforu.domain.model.Article
+import com.example.newsforu.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+
+class NewsRepositoryImpl(
+    private val newsApi: NewsApi
+): NewsRepository {
+    override fun getNews(sources: List<String>): Flow<PagingData<Article>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                NewsPagingSource(
+                    newsApi = newsApi,
+                    sources = sources.joinToString(separator = ",")
+                )
+            }
+        ).flow
+    }
+
+}
