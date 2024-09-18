9e12f3df12e24dc4067e924c54df865acd2d7288
Yug-spec <vidiyala.yugander@viacom18.com>
2024-09-18 14:25:00 +0530

    latest changes

---
+++ app/src/main/java/com/example/newsforu/domain/usecases/AppEntryUseCases.kt
@@ -0,0 +1,6 @@
+package com.example.newsforu.domain.usecases
+
+data class AppEntryUseCases(
+    val readAppEntry: ReadAppEntry,
+    val saveAppEntry: SaveAppEntry
+)
--- app/src/main/java/com/example/newsforu/util/Constants.kt
+++ app/src/main/java/com/example/newsforu/util/Constants.kt
@@ -6,10 +6,13 @@ object Constants {
 
     const val APP_ENTRY = "appEntry"
 
+<<<<<<< HEAD
     const val API_KEY = "a02cb067c3bb483590a6fbbd6ee65e8b"
 
     const val BASE_URL = "https://newsapi.org/v2/"
 
     const val NEWS_DATABASE_NAME = "news_db"
 
+=======
+>>>>>>> 59bb0715eea953f29f952b242a81d3742065879a
 }
